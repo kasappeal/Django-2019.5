82a8488cc43c5ba7a791dc4fa1d2b27a74a89364
Alberto Casero <alberto@kasfactory.net>
2019-05-07 00:33:42 +0200

    Nuestro primer hello world

--- main/urls.py
+++ main/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from photos.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello', hello_world)
 ]
