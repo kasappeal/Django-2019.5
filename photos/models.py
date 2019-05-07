fd2c6990142c126985c297fd5e2ca781594b50d9
Alberto Casero <alberto@kasfactory.net>
2019-05-07 21:45:07 +0200

    Creamos el modelo foto y sus migraciones

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+class Photo(models.Model):
+
+    COPYRIGHT = 'RIG'
+    COPYLEFT = 'LEF'
+    CREATIVE_COMMONS = 'CC'
+
+    LICENSES = [
+        [COPYLEFT, 'Copyleft'],
+        [COPYRIGHT, 'Copyright'],
+        [CREATIVE_COMMONS, 'Creative Commons']
+    ]
+
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField(null=True, blank=True)
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+    license = models.CharField(max_length=3, choices=LICENSES)
