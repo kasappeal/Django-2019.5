79621f894e56cb26c613426274feb5490929e96f
Alberto Casero <alberto@kasfactory.net>
2019-05-07 21:02:07 +0200

    Jugamos con el objeto request capturando parámetros que nos pasan por GET

--- photos/views.py
+++ photos/views.py
@@ -2,4 +2,7 @@ from django.http import HttpResponse
 
 
 def hello_world(request):
-    return HttpResponse('Hello world!')
+    name = request.GET.get('name', 'world')
+    order = request.GET.get('order', 'planet')
+    response = 'Hello {0}, I am a {1}'.format(name, order)
+    return HttpResponse(response)
