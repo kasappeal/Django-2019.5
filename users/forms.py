597e6f6dbd531f94db2d1a64cecf5fbb6ff004ca
Alberto Casero <alberto@kasfactory.net>
2019-05-09 23:49:13 +0200

    Implementamos el formulario de Login utilizando un Django form

---
+++ users/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+
+
+class LoginForm(forms.Form):
+
+    usr = forms.CharField(label='Username')
+    pwd = forms.CharField(label='Password', widget=forms.PasswordInput())
--- users/views.py
+++ users/views.py
@@ -3,20 +3,29 @@ from django.contrib.auth import authenticate
 from django.shortcuts import render, redirect
 from django.contrib.auth import login as django_login, logout as django_logout
 
+from users.forms import LoginForm
+
 
 def login(request):
     if request.user.is_authenticated:
         return redirect('home')
+
     if request.method == 'POST':
-        username = request.POST['usr']
-        password = request.POST['pwd']
-        user = authenticate(username=username, password=password)
-        if user is None:
-            messages.error(request, 'Usuario/contraseña incorrectos')
-        else:
-            django_login(request, user)
-            return redirect('home')
-    return render(request, 'users/login.html')
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('usr')
+            password = form.cleaned_data.get('pwd')
+            user = authenticate(username=username, password=password)
+            if user is None:
+                messages.error(request, 'Usuario/contraseña incorrectos')
+            else:
+                django_login(request, user)
+                return redirect('home')
+    else:
+        form = LoginForm()
+
+    context = {'form': form}
+    return render(request, 'users/login.html', context)
 
 
 def logout(request):
