12a88d19f2c500628270886e2ba881762dd10c00
Alberto Casero <alberto@kasfactory.net>
2019-05-14 22:18:39 +0200

    Añadimos validación anti-palabrotas en el campo descripción

--- photos/forms.py
+++ photos/forms.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.forms import ModelForm
 
 from photos.models import Photo
@@ -8,3 +9,11 @@ class PhotoForm(ModelForm):
     class Meta:
         model = Photo
         fields = ['name', 'url', 'description', 'license', 'visibility']
+
+    def clean_description(self):
+        description = self.cleaned_data.get('description')
+        BADWORDS = ['fuck', 'bastard', 'asshole', 'shit', 'Abollao', 'Limpiatubos', 'Mascachapas']
+        for badword in BADWORDS:
+            if badword.lower() in description.lower():
+                raise ValidationError('{0} is a badword'.format(badword))
+        return description
