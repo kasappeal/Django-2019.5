856164e5c4a496a57cf2b6d8618b08686cc4802f
Alberto Casero <alberto@kasfactory.net>
2019-05-15 21:56:51 +0200

    Personalizamos el listado de fotos en el administrador de Django

--- photos/admin.py
+++ photos/admin.py
@@ -1,5 +1,25 @@
 from django.contrib import admin
+from django.utils.safestring import mark_safe
 
 from photos.models import Photo
 
-admin.site.register(Photo)
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+
+    list_display = ['get_img', 'name', 'license', 'visibility', 'get_owner_name']
+    list_filter = ['license', 'visibility', 'owner']
+    search_fields = ['name', 'description', 'url', 'owner__first_name', 'owner__last_name']
+    readonly_fields = ['get_img', 'get_owner_name']
+
+    def get_owner_name(self, obj):
+        return '{0} {1}'.format(obj.owner.first_name, obj.owner.last_name)
+
+    get_owner_name.short_description = 'Owner'
+    get_owner_name.admin_order_field = 'owner__first_name'
+
+    def get_img(self, obj):
+        return mark_safe('<img src="{0}" height="50">'.format(obj.url))
+
+    get_img.short_description = 'Image'
+    get_img.admin_order_field = 'name'
